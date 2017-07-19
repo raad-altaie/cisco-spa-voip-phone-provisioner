6b688bb0da010350ab6ecde5d6d3e8d8c46c28f6
Raad Altaie <raad.altaie@icloud.com>
2017-07-19 07:51:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
  window.onload = function () {
 
      var ip = document.getElementById('ip');
@@ -13,63 +14,57 @@
          url.style.removeProperty('border');
      };
 
-
-     provision.onclick = function () {
+     function Validate() {
          var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
          var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
          var match1 = ip.value.match(patt1);
          var match2 = ip.value.match(patt2);
-         var lowercase = url.value.toLowerCase();
 
-         if ((match1 || match2) == null) {
+         if (ip.value == "") {
+             alert("Please submit an IP address.");
+             ip.style.borderColor = 'red';
+             return false;
+         } else if ((match1 || match2) == null) {
              alert("Please submit a valid IP address.");
              ip.style.borderColor = 'red';
              return false;
-         } else if (url.value == "") {
-             alert("Please insert the configuration Url");
-             url.style.borderColor = "red";
-             return false;
-         } else if ((lowercase.indexOf('http') == -1) || (lowercase.indexOf('cfg') == -1)) {
-             alert("Please insert a valid configuration Url");
-             url.style.borderColor = "red";
-             return false;
          } else {
-             alert("Your phone may reboot in few seconds")
-             link = (ip.value) + "/admin/resync?" + (url.value)
-             window.open("http://" + link)
+             return true;
          }
      }
 
-     admin.onclick = function () {
-         var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
-         var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-         var match1 = ip.value.match(patt1);
-         var match2 = ip.value.match(patt2);
-         if ((match1 || match2) == null) {
-             alert("Please submit a valid IP address.");
-             ip.style.borderColor = "red";
-             return false;
+     provision.onclick = function () {
+         var lowercase = url.value.toLowerCase();
 
-         } else {
+         if (Validate()) {
+             if (url.value == "") {
+                 alert("Please insert the configuration Url");
+                 url.style.borderColor = "red";
+                 return false;
+             } else if ((lowercase.indexOf('http') == -1) || (lowercase.indexOf('cfg') == -1)) {
+                 alert("Please insert a valid configuration Url");
+                 url.style.borderColor = "red";
+                 return false;
+             } else {
+                 alert("Your phone may reboot in few seconds")
+                 link = (ip.value) + "/admin/resync?" + (url.value)
+                 window.open("http://" + link)
+             }
+         }
+     }
+     admin.onclick = function () {
+         if (Validate()) {
              link = (ip.value) + "/admin/advanced"
              window.open("http://" + link)
          }
      }
 
-     reboot.onclick = function () {
-         var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
-         var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-         var match1 = ip.value.match(patt1);
-         var match2 = ip.value.match(patt2);
 
-         if ((match1 || match2) == null) {
-             alert("Please submit a valid IP address.");
-             ip.style.borderColor = "red";
-             return false;
-         } else {
+     reboot.onclick = function () {
+         if (Validate()) {
              alert("Your phone will reboot now!")
              link = (ip.value) + "/admin/reboot"
              window.open("http://" + link)
          }
+
      }
- }
