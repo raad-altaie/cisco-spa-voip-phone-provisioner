093504a37ac93aa9403b73dede6380cc3e2f1793
Raad Altaie <raad.altaie@icloud.com>
2017-07-19 07:14:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,19 +15,17 @@
 
 
      provision.onclick = function () {
-         var ipValue = document.getElementById('ip').value;
-         var urlValue = document.getElementById('url').value;
          var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
          var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-         var match1 = ipValue.match(patt1);
-         var match2 = ipValue.match(patt2);
-         var lowercase = urlValue.toLowerCase();
+         var match1 = ip.value.match(patt1);
+         var match2 = ip.value.match(patt2);
+         var lowercase = url.value.toLowerCase();
 
          if ((match1 || match2) == null) {
              alert("Please submit a valid IP address.");
              ip.style.borderColor = 'red';
              return false;
-         } else if (urlValue == "") {
+         } else if (url.value == "") {
              alert("Please insert the configuration Url");
              url.style.borderColor = "red";
              return false;
@@ -37,34 +35,32 @@
              return false;
          } else {
              alert("Your phone may reboot in few seconds")
-             link = (ipValue) + "/admin/resync?" + (urlValue)
+             link = (ip.value) + "/admin/resync?" + (url.value)
              window.open("http://" + link)
          }
      }
 
      admin.onclick = function () {
-         var ipValue = document.getElementById('ip').value;
          var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
          var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-         var match1 = ipValue.match(patt1);
-         var match2 = ipValue.match(patt2);
+         var match1 = ip.value.match(patt1);
+         var match2 = ip.value.match(patt2);
          if ((match1 || match2) == null) {
              alert("Please submit a valid IP address.");
              ip.style.borderColor = "red";
              return false;
 
          } else {
-             link = (ipValue) + "/admin/advanced"
+             link = (ip.value) + "/admin/advanced"
              window.open("http://" + link)
          }
      }
 
      reboot.onclick = function () {
-         var ipValue = document.getElementById('ip').value;
          var patt1 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5}$/;
          var patt2 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-         var match1 = ipValue.match(patt1);
-         var match2 = ipValue.match(patt2);
+         var match1 = ip.value.match(patt1);
+         var match2 = ip.value.match(patt2);
 
          if ((match1 || match2) == null) {
              alert("Please submit a valid IP address.");
@@ -72,7 +68,7 @@
              return false;
          } else {
              alert("Your phone will reboot now!")
-             link = (ipValue) + "/admin/reboot"
+             link = (ip.value) + "/admin/reboot"
              window.open("http://" + link)
          }
      }
